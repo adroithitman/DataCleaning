7ac6fecb400977be4e784af613564dcd6ee13bc4
vikrant <vikrantanandsingh@gmail.com>
2018-05-06 23:43:06 +0530

    Data cleaning first draft

---
+++ README.md
@@ -0,0 +1,19 @@
+---
+title: "README"
+author: "Vikrant Singh"
+date: "May 6, 2018"
+output: html_document
+---
+
+## Getting and Cleaning Data - Course Project
+1. Download Zip file & unzip it
+2. Load Activity labels
+3. Load Features Labels
+4. Extract only mean and std Fields - Appropriately labels the data set with descriptive variable names.
+5. Load the train datasets - Extracts only the measurements on the mean and standard deviation for each measurement.
+6. Load the test datasets - Extracts only the measurements on the mean and standard deviation for each measurement.
+7. Merge train and test followed by adding Labels
+8. Uses descriptive activity names to name the activities in the data set
+9. The average of each variable for each activity and each subject is calculated and added.
+10.Tidy data set created in atxt file
+
---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+#Download Zip file & unzip it
+file<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+download.file(file,"dataset.zip")
+unzip("dataset.zip")
+
+
+#Load Activity labels
+activityL <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+#Load Features
+featureL <- read.table("UCI HAR Dataset/features.txt")
+
+#Extract only mean and std Fields
+# Appropriately labels the data set with descriptive variable names.
+
+featureL[,2] <- as.character(featureL[,2])
+requiredFeatures <- grep("*mean*|*std*",featureL[,2])
+requiredFeatures.names <- featureL[requiredFeatures,2]
+requiredFeatures.names <- gsub("-mean","Mean",requiredFeatures.names)
+requiredFeatures.names <- gsub("-std","Std",requiredFeatures.names)
+requiredFeatures.names <- gsub("[()-]","",requiredFeatures.names)
+
+#Load the train datasets
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[requiredFeatures]
+trainAct <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSub <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSub, trainAct, train)
+
+#Load the test datasets
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[requiredFeatures]
+testAct <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSub <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSub, testAct, test)
+
+#Merge train and test followed by adding Labels
+
+final <- rbind(train,test)
+colnames(final)<-c("Subject","Activity",requiredFeatures.names)
+
+# Uses descriptive activity names to name the activities in the data set
+
+
+final$Activity <- factor(final$Activity, levels = activityL[,1], labels = activityL[,2])
+final$Subject <- as.factor(final$Subject)
+
+final.melted <- melt(final, id = c("Subject", "Activity"))
+# the average of each variable for each activity and each subject.
+
+final.mean <- dcast(final.melted, Subject + Activity ~ variable, mean)
+
+# From the data set in step 4, creates a second, independent tidy data set with 
+write.table(final.mean, "tidyData.txt", row.names = FALSE, quote = FALSE)
+
